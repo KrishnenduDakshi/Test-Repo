42b796e8d6b7e8471f589f4c704614a43548293d
krishnendu Dakshi <kdakshi2020@gamil.comi>
2022-02-22 15:40:46 +0530

    Added one more print statemnent

--- hello.java
+++ hello.java
@@ -4,5 +4,6 @@ class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Changes are allowed here !!"); 
+        System.out.println("pushing changes for second time !!"); 
     }
 }
